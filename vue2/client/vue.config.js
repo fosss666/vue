1d4096e8454c35b282eb3ce7e5f91c7f89de330d
fo <1745179058@qq.com>
2022-09-24 20:24:45 +0800

    上传vue2后端代码

--- vue2/client/vue.config.js
+++ vue2/client/vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 7070,
-    proxy: {
+    proxy: {//配置代理，解决跨域问题
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true
