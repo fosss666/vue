7aa436c970edc964bb5eb622cedad0586d12c239
fo <1745179058@qq.com>
2022-09-24 18:37:55 +0800

    vue2单向绑定

--- vue2/client/src/App.vue
+++ vue2/client/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>{{msg}}</h1>
+  <div>
+    <h1>{{msg}}</h1>
+    <div><input type="text" :value="name">简化写法</div>
+    <div><input type="date" v-bind:value="birthday"></div>
+  </div>
+
+
+
 </template>
 
 <script>
 export default{
   data(){
     return {
-      msg:"嘞好啊"
+      msg:"嘞好啊",
+      name:"张三",
+      birthday:'1999-12-12'
     }
   }
 }
