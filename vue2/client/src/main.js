ea92ed705a87cfe2e295799957ed25da17bf2c0d
fo <1745179058@qq.com>
2022-09-25 13:15:54 +0800

    vue2-el-安装引用

--- vue2/client/src/main.js
+++ vue2/client/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 import router from './router'
 import store from './store'
+import example from './views/ExampleDemo1'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(Element)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(example)
 }).$mount('#app')
---
+++ vue2/client/src/views/ExampleDemo1.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1>el</h1>
+  <el-button>我是按钮</el-button>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
