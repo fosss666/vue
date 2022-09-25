6a6769d4072ec68ed70a2212c410556b2368f0bb
fo <1745179058@qq.com>
2022-09-25 21:03:40 +0800

    vue2-el-table表格

--- vue2/client/src/views/ExampleDemo1.vue
+++ vue2/client/src/views/ExampleDemo1.vue
@@ -3,12 +3,52 @@
   <h1>el</h1>
   <el-button>我是按钮</el-button>
 
+  <template>
+    <el-table
+        :data="students"
+        stripe
+        style="width: 100%">
+      <el-table-column
+          prop="id"
+          label="编号"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="sex"
+          label="性别">
+      </el-table-column>
+      <el-table-column
+          prop="age"
+          label="年龄">
+      </el-table-column>
+    </el-table>
+  </template>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return {
+      students:[]
+    }
+  },
+  created() {
+    this.getAll()
+  },
+  methods:{
+    getAll(){
+      axios.get("/api/students").then(res=>{
+        this.students=res.data.data
+      })
+    }
+  }
 }
 </script>
 
