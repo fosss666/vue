e8afa3ea77bceb3b2874f5e14baa0cf4cfcaf3cc
fo <1745179058@qq.com>
2022-09-26 22:38:37 +0800

    vue2-router-嵌套路由

--- vue2/client/src/router/exampleDemo3.js
+++ vue2/client/src/router/exampleDemo3.js
@@ -7,41 +7,62 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  /**
-   * 静态导入
-   */
-  /*{
-    path:'/',
-    component:ContainerView
-  },
-  {
-    path:'/login',
-    component:LoginView
-  },
-  {
-    path:'/404',
-    component:NotFoundView
-  },*/
+    /**
+     * 静态导入
+     */
+    /*{
+      path:'/',
+      component:ContainerView
+    },
+    {
+      path:'/login',
+      component:LoginView
+    },
+    {
+      path:'/404',
+      component:NotFoundView
+    },*/
 
-  /**
-   * 动态导入
-   */
-  {
-    path:'/',
-    component:()=>import("@/views/example14/ContainerView")
-  },
-  {
-    path:'/login',
-    component:()=>import('@/views/example14/LoginView')
-  },
-  {
-    path:'/404',
-    component:()=>import('@/views/example14/NotFoundView')
-  },
+    /**
+     * 动态导入
+     */
+    {
+        path: '/',
+        component: () => import("@/views/example14/ContainerView"),
+        redirect:'/c1/p1',
+        children: [
+            {
+                path: 'c1/p1',
+                component: () => import('@/views/example14/container/P1View')
+            },
+            {
+                path: 'c1/p2',
+                component: () => import('@/views/example14/container/P2View')
+            },
+            {
+                path: 'c1/p3',
+                component: () => import('@/views/example14/container/P3View')
+            },
+
+        ],
+
+    },
+    {
+        path: '/login',
+        component: () => import('@/views/example14/LoginView')
+    },
+    {
+        path: '/404',
+        component: () => import('@/views/example14/NotFoundView')
+    },
+    {
+        path:'*',
+        redirect: '/404'
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
