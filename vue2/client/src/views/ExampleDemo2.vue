e6facb473ff4f6cb2df5ac140ca4b8d5fcd039c0
fo <1745179058@qq.com>
2022-09-26 10:48:50 +0800

    vue2-el-级联选择器

--- vue2/client/src/views/ExampleDemo2.vue
+++ vue2/client/src/views/ExampleDemo2.vue
@@ -18,16 +18,18 @@ export default {
       axios.get("/api/menu").then(res=>{
         const map=new Map();
         const menus=res.data.data;
+        console.log(menus)
         for(const {id,name,pid} of menus){
           map.set(id,{value:id,label:name,pid:pid});
         }
+        console.log(map)
 
         //设置父子关系
         const values=map.values()
         const top=[]//顶层
         for( const value of values){
           const parent=map.get(value.pid)
-          if(parent!==undefined){
+          if(parent!==undefined){//parent已经初始化过
             if(parent.children===undefined){
               parent.children=[]
             }
