a9bb482452133e42a9fba7b58688f7091cdb9018
fo <1745179058@qq.com>
2022-09-26 22:19:18 +0800

    vue2-router-动态导入（没有用到的组件就不进行加载）

--- vue2/client/src/router/exampleDemo3.js
+++ vue2/client/src/router/exampleDemo3.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-import ContainerView from "@/views/example14/ContainerView";
-import LoginView from '@/views/example14/LoginView'
-import NotFoundView from '@/views/example14/NotFoundView';
+// import ContainerView from "@/views/example14/ContainerView";
+// import LoginView from '@/views/example14/LoginView'
+// import NotFoundView from '@/views/example14/NotFoundView';
 Vue.use(VueRouter)
 
 const routes = [
-  {
+  /**
+   * 静态导入
+   */
+  /*{
     path:'/',
     component:ContainerView
   },
@@ -18,6 +21,22 @@ const routes = [
   {
     path:'/404',
     component:NotFoundView
+  },*/
+
+  /**
+   * 动态导入
+   */
+  {
+    path:'/',
+    component:()=>import("@/views/example14/ContainerView")
+  },
+  {
+    path:'/login',
+    component:()=>import('@/views/example14/LoginView')
+  },
+  {
+    path:'/404',
+    component:()=>import('@/views/example14/NotFoundView')
   },
 ]
 
