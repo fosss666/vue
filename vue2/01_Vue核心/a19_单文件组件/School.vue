f77beedb823c47b6e8018a93da570e955de9e005
fosss <1745179058@qq.com>
2023-09-08 21:14:20 +0800

    单文件组件结构

---
+++ vue2/01_Vue核心/a19_单文件组件/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}}</h2>
+    <button @click="show">点我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School", //显示在开发者工具中的标签名称
+  data() {
+    return {
+      name: "尚硅谷",
+    };
+  },
+  methods: {
+    show() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+<style>
+.demo {
+  background-color: rgb(13, 13, 176);
+}
+</style>
---
+++ vue2/01_Vue核心/a19_单文件组件/Student.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>学生名称：{{ name }}}</h2>
+    <button @click="show">点我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student", //显示在开发者工具中的标签名称
+  data() {
+    return {
+      name: "张三",
+    };
+  },
+  methods: {
+    show() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+<style>
+.demo {
+  background-color: rgb(226, 122, 25);
+}
+</style>
