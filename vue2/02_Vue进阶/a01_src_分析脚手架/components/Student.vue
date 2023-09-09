9d72ba92749f25e890f84e520bb9851f39821cce
fosss <1745179058@qq.com>
2023-09-09 22:13:27 +0800

    分析vue脚手架

---
+++ vue2/02_Vue进阶/a01_src_分析脚手架/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "张三",
+      age: 18,
+    };
+  },
+};
+</script>
+<style>
+.demo {
+  background-color: rgb(255, 0, 0);
+}
+</style>
