421c3ccfeaca2f7d8db69f93269354ba7c92ef8e
fosss <1745179058@qq.com>
2023-09-13 21:37:32 +0800

    TodoList案例-静态

---
+++ vue2/02_Vue进阶/src/components/MyItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" />
+      <span>xxxxx</span>
+    </label>
+    <button class="btn btn-danger" style="display: none">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "MyItem",
+};
+</script>
+
+<style>
+</style>
---
+++ vue2/02_Vue进阶/src/components/MyList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ul class="todo-main">
+      <MyItem />
+      <MyItem />
+      <MyItem />
+      <MyItem />
+    </ul>
+  </div>
+</template>
+
+<script>
+import MyItem from "./MyItem.vue";
+export default {
+  name: "MyList",
+  components: {
+    MyItem,
+  },
+};
+</script>
+
+<style>
+</style>
