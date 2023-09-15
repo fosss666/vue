8de03638924c29491db60a2d1328b05bf5d90160
fosss <1745179058@qq.com>
2023-09-15 21:18:50 +0800

    todoList案例-初始化列表

--- vue2/02_Vue进阶/src/components/MyList.vue
+++ vue2/02_Vue进阶/src/components/MyList.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
     <ul class="todo-main">
-      <MyItem />
-      <MyItem />
-      <MyItem />
-      <MyItem />
+      <MyItem v-for="todo in todos" :key="todo.id" :todo="todo"/>
     </ul>
   </div>
 </template>
@@ -15,6 +12,15 @@ export default {
   name: "MyList",
   components: {
     MyItem,
+  },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "吃饭", done: true },
+        { id: "002", title: "睡觉", done: false },
+        { id: "003", title: "打豆豆", done: true },
+      ],
+    };
   },
 };
 </script>
