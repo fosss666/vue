2a18d4cba251e329d86fb5275ca4afade8080879
fosss <1745179058@qq.com>
2023-09-22 21:54:26 +0800

    浏览器本地存储

---
+++ vue2/02_Vue进阶/a07_src_TodoList案例/components/MyList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <ul class="todo-main">
+      <MyItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        :changeDone="changeDone"
+        :deleteTodo="deleteTodo"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import MyItem from "./MyItem.vue";
+export default {
+  name: "MyList",
+  props: ["todos", "changeDone", "deleteTodo"],
+  components: {
+    MyItem,
+  },
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
