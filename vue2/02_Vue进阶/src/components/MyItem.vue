6e9a4eabb6a20bd4edfd5fb670878c70932d0095
fosss <1745179058@qq.com>
2023-09-27 22:03:24 +0800

    TodoList案例-全局事件总线

---
+++ vue2/02_Vue进阶/src/components/MyItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <li>
+    <label>
+      <input
+        type="checkbox"
+        :checked="todo.done"
+        @click="changeChecked(todo.id)"
+      />
+      <span>{{ todo.title }}</span>
+    </label>
+    <button class="btn btn-danger" @click="deleteChecked(todo.id)">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "MyItem",
+  props: ["todo"],
+  methods: {
+    //更改数据的勾选状态
+    changeChecked(id) {
+      //调用事件
+      this.$bus.$emit("changeDone", id);
+    },
+    //删除指定todo
+    deleteChecked(id) {
+      if (confirm("确定要删除吗？")) {
+        this.$bus.$emit("deleteTodo", id);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+li :hover {
+  background-color: #ddd;
+}
+li:hover button {
+  display: block;
+}
+</style>
--- vue2/02_Vue进阶/src/main.js
+++ vue2/02_Vue进阶/src/main.js
@@ -1,12 +1,12 @@
-//引入Vue
 import Vue from 'vue'
-//引入App
 import App from './App.vue'
-//关闭Vue的生产提示
+
 Vue.config.productionTip = false
 
-//创建vm
 new Vue({
-	el: '#app',
-	render: h => h(App)
-})
+  render: h => h(App),
+  beforeCreate() {
+    //安装事件总线
+    Vue.prototype.$bus = this
+  },
+}).$mount('#app')
